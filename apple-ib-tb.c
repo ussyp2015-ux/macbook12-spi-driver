4642407814467f9b77cabc90f768198a781a2eb9
Usaamah Patel <ussyp2015@gmail.com>
2025-11-15 14:31:03 +0000

    Fix report type handling and change platform remove function return type

--- apple-ib-tb.c
+++ apple-ib-tb.c
@@ -953,6 +953,8 @@ static int appletb_fill_report_info(struct appletb_device *tb_dev,
 		report_info->report_type = 0x02; break;
 	case HID_FEATURE_REPORT:
 		report_info->report_type = 0x03; break;
+	default:
+		report_info->report_type = 0x00; break;
 	}
 
 	return 1;
@@ -1259,7 +1261,7 @@ error:
 	return rc;
 }
 
-static int appletb_platform_remove(struct platform_device *pdev)
+static void appletb_platform_remove(struct platform_device *pdev)
 {
 	struct appleib_device_data *ddata = pdev->dev.platform_data;
 	struct appleib_device *ib_dev = ddata->ib_dev;
@@ -1268,14 +1270,9 @@ static int appletb_platform_remove(struct platform_device *pdev)
 
 	rc = appleib_unregister_hid_driver(ib_dev, &appletb_hid_driver);
 	if (rc)
-		goto error;
+		dev_warn(ddata->log_dev, "failed to unregister hid driver: %d\n", rc);
 
 	appletb_free_device(tb_dev);
-
-	return 0;
-
-error:
-	return rc;
 }
 
 static const struct platform_device_id appletb_platform_ids[] = {
